6a9f0f697bc6a415fedecbe290343b740d2e5d71
itoutsource.cz10@trinasolar.com <itoutsource.cz10@trinasolar.com>
2020-10-14 08:21:21 +0800

    html模仿学习

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,6 +45,52 @@
         });
     }
 
+    let $blogList = $(".blog-list");
+    if ($blogList.length > 0) {
+        console.log('$blogList.length--->' + $blogList.length);
+        $blogList.owlCarousel({
+            item: 3,
+            loop: true,
+            nav: true,
+            dots: false,
+            navText: ['<i class="ion-ios-arrow-left"></i>', '<i class="ion-ios-arrow-right"></i>'],
+            responsiveClass: true,
+            responsive: {
+                0: {item: 1},
+                400: {item: 2},
+                800: {item: 3}
+            }
+        })
+    }
+
+    let $categoriesList = $(".categories-list");
+    if ($categoriesList.length > 0) {
+        $(".categories-list").on('changed.owl.carousel initialized.owl.carousel', function (event) {
+
+        }).owlCarousel({
+            items: 4,
+            loop: true,
+            nav: true,
+            navText: ['<i class="ion-ios-arrow-left"></i>', '<i class="ion-ios-arrow-right"></i>'],
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                480: {
+                    items: 2,
+                },
+                576: {
+                    items: 3,
+                },
+                768: {
+                    items: 4,
+                }
+            }
+        });
+    }
+
+
     const $brandList = $(".brand-list");
     if ($brandList.length > 0) {
         //初始化  发生改变
@@ -83,5 +129,37 @@
         });
     }
 
+    let $thumbColumn1 = $(".testimonial-thumb-column1");
+    $thumbColumn1.slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.testimonial-thumb-column3',
+        nextArrow: '<button class="slick-next"> pre </button>',
+        prevArrow: '<button class="slick-prev"> next </button>',
+    });
+
+    let $thumbColumn3 = $(".testimonial-thumb-column3");
+    $thumbColumn3.slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 3,
+        asNavFor: '.testimonial-thumb-column1',
+        centerMode: true,
+        focusOnSelect: true
+    });
+
+    $thumbColumn3.on('afterChange', function (event, slick, currentSlide, nextSlide) {
+        console.log(currentSlide.toString());
+    });
+
+    //popwindow
+    setTimeout(function () {
+        console.log("-----")
+        $('.pop-view').bPopup();
+
+
+    },1000)
 
 })(jQuery);
